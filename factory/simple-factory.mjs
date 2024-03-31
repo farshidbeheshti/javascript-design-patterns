c02aebd2d2858d179ea32318e0d8b6b1ba746da0
Farshid Beheshti <farshid.beheshti@gmail.com>
2024-03-31 17:41:21 +0330

    fix: making the Simple Factory pattern not constructable

--- factory/simple-factory.mjs
+++ factory/simple-factory.mjs
@@ -1,4 +1,13 @@
 export class Factory {
+  constructor() {
+    if (new.target == Factory)
+      throw new TypeError(
+        `
+        It seems that this simple factory does not need a contractor,
+        so we prevent instantiating an instance of the it
+        `.replace(/\s+/g, " ")
+      );
+  }
   static createInstance(options) {
     const { type, ...args } = options;
     // TODO: Check if `type` exists in FactoryTypes
